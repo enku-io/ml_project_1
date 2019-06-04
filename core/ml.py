f9ac95a2921a12fa090dd8aabdd62ff4ab5b5776
enku2015 <enku.wende@gmail.com>
2019-06-03 22:47:10 -0500

    Initial Commit

---
+++ core/ml.py
@@ -0,0 +1,31 @@
+import tensorflow as tf
+import numpy as np # linear algebra
+import pandas as pd
+from core.preprocess import load_dataset
+
+
+
+def train_model(X_train, X_test, y_train, y_test):
+    model = tf.keras.models.Sequential([
+      tf.keras.layers.Dense(12, activation=tf.nn.relu, input_shape=[len(X_train.keys())]),
+      tf.keras.layers.Dense(1, activation=tf.nn.sigmoid)
+    ])
+    model.compile(optimizer='adam',
+                  loss='binary_crossentropy',
+                  metrics=['accuracy'])
+
+    model.fit(X_train, y_train, epochs=100)
+    model.save('my_model.h5')
+    l, a = model.evaluate(X_test, y_test)
+    print("Accuracy: ", a)
+
+def predict_model(X_test,y_test):
+    nn_y_pred = model.predict(X_test)
+
+    nn_fpr, nn_tpr, thresholds = metrics.roc_curve(y_test, nn_y_pred)
+    print("AUC: ", metrics.auc(nn_fpr, nn_tpr))
+
+if __name__ == '__main__':
+    X_train, X_test, y_train, y_test = load_dataset()
+    train_model(X_train, X_test, y_train, y_test)
+    predict_model(X_test,y_test)
---
+++ core/preprocess.py
@@ -0,0 +1,58 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn import preprocessing
+
+def load_dataset():
+    df = pd.read_csv("data/insurance.csv")
+    df = transform_features(df)
+    df = encode_features(df)
+    X_train, X_test, y_train, y_test = split_data(df)
+    return X_train, X_test, y_train, y_test
+
+def simplify_ages(data):
+    bins = (15, 20, 30, 40, 50, 60, 70)
+    group_names = ['Teenager', 'Twenties', 'Thirties', 'Fourties', 'Fifties', 'Sixities']
+    categories = pd.cut(data.age, bins, labels=group_names)
+    data.age = categories
+    return data
+
+def simplify_bmi(data):
+    bins = (15, 25, 35, 45, 60)
+    group_names = ['Small', 'Medium', 'Big', 'vBig']
+    categories = pd.cut(data.bmi, bins, labels=group_names)
+    data.bmi = categories
+    return data
+
+def encode_features(data):
+    features = ['age', 'sex', 'bmi', 'smoker', 'region']
+
+    for feature in features:
+        le = preprocessing.LabelEncoder()
+        le = le.fit(data[feature])
+        data[feature] = le.transform(data[feature])
+    return data
+
+def categorize(data):
+    data['approved'] = 0
+
+    for index, row in data.iterrows():
+        if row['charges'] < 16639.912515:
+            data.at[index, 'approved'] = 1
+        else:
+            data.at[index, 'approved'] = 0
+    return data
+
+def split_data(data):
+
+    X_all = data.drop(['approved', 'charges'], axis=1)
+    y_all = data['approved']
+
+    num_test = 0.20
+    X_train, X_test, y_train, y_test = train_test_split(X_all, y_all, test_size=num_test, random_state=12)
+
+    return X_train, X_test, y_train, y_test
+
+def transform_features(data):
+    data = simplify_ages(data)
+    data = simplify_bmi(data)
+    return data
